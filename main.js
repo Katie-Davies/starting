15112934ae9745ae9c100b48cb542abce9f6479b
Katie Davies <millerkatie1990@gmail.com>
2024-05-16 12:34:22 +1200

    add create field function

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class Field {
     this.field = field
     this.locationX = 0
     this.locationY = 0
+    this.field[0][0] = pathCharacter
   }
   print() {
     this.field.forEach((element) => {
@@ -21,6 +22,21 @@ class Field {
     while (playing) {
       this.print()
       this.askQuestion()
+      if (!this.isInBounds) {
+        console.log('out of bounds')
+        playing = false
+        break
+      } else if (this.isHole()) {
+        console.log('You fell down a hole')
+        playing = false
+        break
+      } else if (this.isHat()) {
+        console.log('Yay you found your hat!')
+        playing = false
+        break
+      }
+      //update current location on map
+      this.field[this.locationY][this.locationX] = pathCharacter
     }
   }
   //prompt questions
@@ -57,23 +73,42 @@ class Field {
 
   //if a hat
   isHat() {
-    return this.field[this.locationX][this.locationY] === hat
+    return this.field[this.locationY][this.locationX] === hat
   }
 
   //if hole
   isHole() {
-    return this.field[this.locationX][this.locationY] === hole
+    return this.field[this.locationY][this.locationX] === hole
+  }
+
+  static generateField(height, width, percentage = 0.1) {
+    const field = new Array(height).fill(0).map((el) => new Array(width))
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const prob = Math.random()
+        field[y][x] = prob > percentage ? fieldCharacter : hole
+      }
+    }
+    // Set the "hat" location
+    const hatLocation = {
+      x: Math.floor(Math.random() * width),
+      y: Math.floor(Math.random() * height),
+    }
+    // Make sure the "hat" is not at the starting point
+    while (hatLocation.x === 0 && hatLocation.y === 0) {
+      hatLocation.x = Math.floor(Math.random() * width)
+      hatLocation.y = Math.floor(Math.random() * height)
+    }
+    field[hatLocation.y][hatLocation.x] = hat
+    return field
   }
 }
 
-const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
-])
+// const myField = new Field([
+//   ['*', '░', 'O'],
+//   ['░', 'O', '░'],
+//   ['░', '^', '░'],
+// ])
 
-console.log(myField.print())
-let userMove = prompt('which direction would you like to move?')
-if (userMove) {
-  myField.updateLocation(userMove)
-}
+const myField = new Field(Field.generateField(10, 10, 0.3))
+myField.runGame()
